b4505cb916661ee35a4fa4e188b1cfaf252ccc70
Juan-David-Obando-Novoa <111014101+Juan-David-Obando-Novoa@users.noreply.github.com>
2023-11-06 14:45:43 -0500

    Req 2 proyecto 2

--- src/main/java/uniandes/edu/co/proyecto/Repositorio/RFC2Repository.java
+++ src/main/java/uniandes/edu/co/proyecto/Repositorio/RFC2Repository.java
@@ -10,15 +10,13 @@ import uniandes.edu.co.proyecto.Modelo.Reserva_Servicio;
 
 public interface RFC2Repository extends JpaRepository<Reserva_Servicio, Long>{
 
-    @Query(value = "--Requerimeinto 2\r\n" + 
-            "SELECT s.nombre AS servicio, SUM(rs.cont_servicios) AS total_consumido\r\n" + 
-            "FROM reservas_servicios rs\r\n" + 
-            "INNER JOIN servicios s ON rs.servicios_id = s.id\r\n" + 
-            "INNER JOIN reservas r ON rs.reservas_id = r.id\r\n" + 
-            "WHERE r.fecha_entrada > :fechaI AND r.fecha_salida < :fechaO\r\\n" + //
-                    " " + 
-            "GROUP BY s.nombre\r\n" + 
-            "ORDER BY total_consumido DESC\r\n" + 
+    @Query(value = "SELECT s.nombre AS servicio, SUM(rs.cont_servicios) AS total_consumido " + 
+            "FROM reservas_servicios rs " + 
+            "INNER JOIN servicios s ON rs.servicios_id = s.id " + 
+            "INNER JOIN reservas r ON rs.reservas_id = r.id " + 
+            "WHERE r.fecha_entrada >  TO_DATE(:fechaI, 'YYYY-MM-DD') AND r.fecha_salida < TO_DATE(:fechaO,'YYYY-MM-DD')" +
+            "GROUP BY s.nombre " + 
+            "ORDER BY total_consumido DESC " + 
             "FETCH FIRST 20 ROWS ONLY", nativeQuery = true )
             Collection<Object[]> darRta(@Param("fechaI") String fechaInicial, @Param("fechaO") String fechaFinal);
 
