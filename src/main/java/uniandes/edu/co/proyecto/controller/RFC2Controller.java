8ac43cfda914e81aa901ed3ccdb63a877c21a5b7
Juan-David-Obando-Novoa <111014101+Juan-David-Obando-Novoa@users.noreply.github.com>
2023-11-06 16:01:06 -0500

    prueba2

--- src/main/java/uniandes/edu/co/proyecto/controller/RFC2Controller.java
+++ src/main/java/uniandes/edu/co/proyecto/controller/RFC2Controller.java
@@ -8,13 +8,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import uniandes.edu.co.proyecto.Repositorio.RFC2Repository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 
 @Controller
 public class RFC2Controller {
-    private static final Logger logger = LoggerFactory.getLogger(RFC2Controller.class);
 
     @Autowired
     private RFC2Repository rfc2Repository;
@@ -28,30 +26,6 @@ public class RFC2Controller {
     public String preRfc2Save(Model model, @RequestParam(value = "fechaI") String fechaI, @RequestParam(value = "fechaO") String fechaO) {
         model.addAttribute("fechaI", fechaI);
         model.addAttribute("fechaO", fechaO);
-        logger.info("Este es un mensaje informativo                                  \n aaaaaaaaaaaaaaaaaaaaaaa" + fechaI);
-
-        System.out.println("Fecha de entrada: " + fechaI);
-        System.out.println("Fecha de entrada: " + fechaO);
-
-        LocalDate fechaInicial = LocalDate.parse(fechaI);
-        LocalDate fechaFinal = LocalDate.parse(fechaO);
-        System.out.println("Fecha de con localDate: " + fechaInicial);
-        System.out.println("Fecha de entrada localDate: " + fechaFinal);
-        System.out.println("aaaaaaaaaaaaaaaaaaaaaaa");
-        System.out.println("aaaaaaaaaaaaaaaaaaaaaaa");
-        System.out.println("aaaaaaaaaaaaaaaaaaaaaaa");
-        System.out.println("aaaaaaaaaaaaaaaaaaaaaaa");
-        System.out.println("aaaaaaaaaaaaaaaaaaaaaaa");
-
-
-
-        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yy");
-        String fechaFormateadaI = fechaInicial.format(formato);
-        System.out.println(fechaFormateadaI);
-        System.out.println(fechaFormateadaI);
-        System.out.println("aaaaaaaaaaaaaaaaaaaaaaa");
-
-        String fechaFormateadaO = fechaFinal.format(formato);
         long tiempoFin = System.nanoTime();
         Collection<Object[]> rta = rfc2Repository.darRta(fechaI, fechaO);
         long tiempoInicio = System.nanoTime();
