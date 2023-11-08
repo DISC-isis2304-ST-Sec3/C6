2ef058b23a7023707b65427e3c5951b71be7af73
Juan-David-Obando-Novoa <111014101+Juan-David-Obando-Novoa@users.noreply.github.com>
2023-11-08 11:26:06 -0500

    Final

--- src/main/java/uniandes/edu/co/proyecto/Repositorio/RFC1Repository.java
+++ src/main/java/uniandes/edu/co/proyecto/Repositorio/RFC1Repository.java
@@ -12,23 +12,22 @@ public interface RFC1Repository extends JpaRepository<Reserva, Long>{
 
 
     @Query(value = "SELECT\r\n" + 
-            "    h.id AS habitacion_id,\r\n" + 
-            "    s.nombre AS servicio,\r\n" + 
-            "    SUM(s.precio) AS dinero_recolectado\r\n" + 
-            "FROM\r\n" + 
-            "    habitaciones h\r\n" + 
-            "JOIN\r\n" + 
-            "    reservas r ON h.id = r.habitaciones_id\r\n" + 
-            "JOIN\r\n" + 
-            "    reservas_servicios rs ON r.id = rs.reservas_id\r\n" + 
-            "JOIN\r\n" + 
-            "    servicios s ON rs.servicios_id = s.id\r\n" + 
-            "WHERE\r\n" + 
-            "    r.fecha_entrada >= (SELECT CURRENT_DATE-365 from dual)\r\n" + 
-            "GROUP BY\r\n" + 
-            "    h.id, s.nombre\r\n" +
-            "ORDER BY\r\n" +
-            "    h.id, s.nombre"
+                    "    h.id AS habitacion_id,\r\n" + 
+                    "    SUM(s.precio) AS dinero_recolectado\r\n" + 
+                    "FROM\r\n" + 
+                    "    habitaciones h\r\n" + 
+                    "JOIN\r\n" + 
+                    "    reservas r ON h.id = r.habitaciones_id\r\n" + 
+                    "JOIN\r\n" + 
+                    "    reservas_servicios rs ON r.id = rs.reservas_id\r\n" + 
+                    "JOIN\r\n" + 
+                    "    servicios s ON rs.servicios_id = s.id\r\n" + 
+                    "WHERE\r\n" + 
+                    "    r.fecha_entrada >= SYSDATE - 365\r\n" + 
+                    "GROUP BY\r\n" + 
+                    "    h.id\r\n" + 
+                    "ORDER BY\r\n" + 
+                    "    h.id"
                     , nativeQuery = true )
                     Collection<Object[]> darConsumos();
 
