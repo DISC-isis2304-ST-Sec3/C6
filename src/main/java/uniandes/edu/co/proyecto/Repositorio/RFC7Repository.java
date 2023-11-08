4fc0f06cc672b652c6cefcc8ddf6fe814dd627b0
Juan-David-Obando-Novoa <111014101+Juan-David-Obando-Novoa@users.noreply.github.com>
2023-11-08 00:09:34 -0500

    FInal

--- src/main/java/uniandes/edu/co/proyecto/Repositorio/RFC7Repository.java
+++ src/main/java/uniandes/edu/co/proyecto/Repositorio/RFC7Repository.java
@@ -9,26 +9,18 @@ import uniandes.edu.co.proyecto.Modelo.Reserva;
 
 public interface RFC7Repository extends JpaRepository <Reserva, Long>{
     
-    @Query(value = "SELECT consumo.usuario_id, consumo.usuario_login, estadia.total_dias_estadia, total_consumido\r\n" + //
-            "FROM (\r\n" + 
-            "    -- Resultado del cálculo de consumo\r\n" + 
-            "    SELECT u.id AS usuario_id, u.login AS usuario_login, SUM(r.costo_total) AS total_consumido\r\n" + 
-            "    FROM usuarios u\r\n" + 
-            "    JOIN reservas r ON u.id = r.usuarios_id\r\n" + 
-            "    WHERE r.fecha_entrada >= TO_DATE(SYSDATE - 365, 'DD/MM/YYYY')\r\n" + 
-            "    GROUP BY u.id, u.login\r\n" + 
-            "    HAVING SUM(r.costo_total) > 150000\r\n" + 
-            ") consumo\r\n" + 
-            "JOIN (\r\n" + 
-            "    -- Resultado del cálculo de duración de estadía\r\n" + 
-            "    SELECT u.id AS usuario_id, u.login AS usuario_login, SUM(ABS(r.fecha_salida - r.fecha_entrada)) AS total_dias_estadia\r\n" + 
-            "    FROM usuarios u\r\n" + 
-            "    JOIN reservas r ON u.id = r.usuarios_id\r\n" + 
-            "    WHERE r.fecha_entrada >= TO_DATE(SYSDATE - 365, 'DD/MM/YYYY') and r.fecha_entrada <= (SELECT CURRENT_DATE from dual)\r\n" + 
-            "    GROUP BY u.id, u.login\r\n" + 
-            "    HAVING SUM(ABS(r.fecha_salida - r.fecha_entrada)) >= 14\r\n" + 
-            ") estadia\r\n" + 
-            "ON consumo.usuario_id = estadia.usuario_id\r\n" + 
+    @Query(value = "SELECT u.login AS usuario,\r\n" + 
+            "       r.Usuarios_id AS usuario_id,\r\n" + 
+            "       SUM(rs.cont_servicios * s.precio) AS consumo_total,\r\n" + 
+            "       SUM(r.fecha_salida - r.fecha_entrada) AS total_dias_en_hotel\r\n" + 
+            "FROM reservas r\r\n" + 
+            "INNER JOIN usuarios u ON r.Usuarios_id = u.id\r\n" + 
+            "LEFT JOIN reservas_servicios rs ON r.id = rs.reservas_id\r\n" + 
+            "LEFT JOIN servicios s ON rs.servicios_id = s.id\r\n" + 
+            "WHERE r.fecha_entrada >= TO_DATE(SYSDATE - 365, 'YYYY-MM-DD')  -- Filtrar reservas del último año\r\n" + 
+            "GROUP BY u.login, r.Usuarios_id\r\n" + 
+            "HAVING SUM(r.fecha_salida - r.fecha_entrada) >= 14 OR \r\n" + 
+            "       SUM(rs.cont_servicios * s.precio) > 150000\r\n" + 
             "", nativeQuery = true )
             Collection<Object[]> darRta();
 }
